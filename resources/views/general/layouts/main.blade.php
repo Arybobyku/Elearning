9c2860623b4a07e2e053b70a35eb3af0c8226947
arybobysiregar <aryboby1@gmail.com>
2022-10-01 14:19:33 +0700

    rollback to vite

--- resources/views/general/layouts/main.blade.php
+++ resources/views/general/layouts/main.blade.php
@@ -4,7 +4,8 @@
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link rel="stylesheet" href="path/to/font-awesome/css/font-awesome.min.css">
-  <link href="build/assets/app.55fd2f6d.css">
+
+  @vite('resources/css/app.css')
   
 </head>
 <body>
@@ -135,52 +136,7 @@
 
 
 
-<script>
-  //carousel
-let defaultTransform = 0;
-function goNext() {
-    defaultTransform = defaultTransform - 398;
-    var slider = document.getElementById("slider");
-    if (Math.abs(defaultTransform) >= slider.scrollWidth / 1.7)
-        defaultTransform = 0;
-    slider.style.transform = "translateX(" + defaultTransform + "px)";
-}
-next.addEventListener("click", goNext);
-function goPrev() {
-    var slider = document.getElementById("slider");
-    if (Math.abs(defaultTransform) === 0) defaultTransform = 0;
-    else defaultTransform = defaultTransform + 398;
-    slider.style.transform = "translateX(" + defaultTransform + "px)";
-}
-prev.addEventListener("click", goPrev);
-
-//hamburger
-const hamburger = document.querySelector("#hamburger");
-const navMenu = document.querySelector("#nav-menu");
-
-hamburger.addEventListener("click", function () {
-    hamburger.classList.toggle("hamburger-active");
-    navMenu.classList.toggle("hidden");
-});
-const navProfile = document.querySelector("#nav-profile");
-const profileNav = document.querySelector("#profilenav");
-
-profileNav.addEventListener("click", function () {
-    navProfile.classList.toggle("hidden");
-});
-//navbar fixed
-window.onscroll = function () {
-    const header = document.querySelector("header");
-    const navfix = header.offsetTop;
-
-    if (window.pageYOffset > navfix) {
-        header.classList.add("navbar-fixed");
-    } else {
-        header.classList.remove("navbar-fixed");
-    }
-};
-
-</script>
+<script></script>
 
     <script>
         var botmanWidget = {
