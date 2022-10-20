81f4c0bd8b8aeb3039afa87a79ec165a6070a035
arybobysiregar <aryboby1@gmail.com>
2022-10-20 11:43:22 +0700

    Fix Conflict

--- resources/views/general/layouts/main.blade.php
+++ resources/views/general/layouts/main.blade.php
@@ -61,128 +61,133 @@
                     @endauth
                 @endif
 
-    <div class="flex items-center right-4">
-      <button id="hamburger" name="hamburger" type="button" class="block absolute right-6 lg:hidden">
-        <span class="hamburger-line transition duration-500 ease-in-out origin-bottom-left"></span>
-        <span class="hamburger-line transition duration-500 ease-in-out"></span>
-        <span class="hamburger-line transition duration-500 ease-in-out origin-top-left "></span>
-      </button>
+                <div class="flex items-center right-4">
+                    <button id="hamburger" name="hamburger" type="button" class="block absolute right-6 lg:hidden">
+                        <span class="hamburger-line transition duration-500 ease-in-out origin-bottom-left"></span>
+                        <span class="hamburger-line transition duration-500 ease-in-out"></span>
+                        <span class="hamburger-line transition duration-500 ease-in-out origin-top-left "></span>
+                    </button>
                     <nav id="nav-menu"
                         class="hidden absolute py-5 bg-white shadow-lg rounded-lg max-w-[250px] w-full right-4 top-full lg:block lg:static lg:bg-transparent lg:max-w-full lg:shadow-none lg:rounded-none">
-        <ul class="block lg:flex">
-            <li class="group">
+                        <ul class="block lg:flex">
+                            <li class="group">
                                 <a href="/"
                                     class="text-base text-black py-2 mx-8 group-hover:text-cyan-900">Beranda</a>
-            </li>
-            <li class="group">
+                            </li>
+                            <li class="group">
                                 <a href="/#about"
                                     class="text-base text-black py-2 mx-8 group-hover:text-cyan-900">Tentang</a>
-            </li>
-            <li class="group">
+                            </li>
+                            <li class="group">
                                 <a href="/about"
                                     class="text-base text-black py-2 mx-8 group-hover:text-cyan-900">Berita</a>
-            </li>
-            @if (Route::has('login'))
-            @auth
-             <li class="group">
+                            </li>
+                            @if (Route::has('login'))
+                                @auth
+                                    <li class="group">
                                         <a href="/academies"
                                             class="text-base text-black py-2 mx-8 group-hover:text-cyan-900">Akademi</a>
-            </li>
-@endauth
-                    @endif
-           
-        </ul>
-      </nav>
-    </div>
-  </div>
-</div>
-</header>
-      <div class="pt-32">
+                                    </li>
+                                @endauth
+                            @endif
+
+                        </ul>
+                    </nav>
+                </div>
+            </div>
+        </div>
+    </header>
+    <div class="pt-32">
         @yield('contents')
-      </div>
+    </div>
 
-     <section id="contact">
+    <section id="contact">
 
-   <div class="py-16 min-w-full flex transition-all duration-500 bg-slate-700 pt-30">
-      <div class="container max-w-screen-lg mx-auto">
-        <div class="grid grid-cols-1 sm:grid-cols-2 gap-4 content-center">
-          <div class="">
-            <p class=" font-light text-white mb-4">
-              Thank you for your attention.
-            </p>
-             <p class=" font-light text-white">
-              Sumatera Utara Ibukotanya Medan<br />
-              Tempat Bermukim Sultan Deli<br />
-              Terima Kasih Kawan" Sekalian<br />
-              Semoga Kita Dapat Berjumpa Nanti<br />
-            </p>
-          </div>
+        <div class="py-16 min-w-full flex transition-all duration-500 bg-slate-700 pt-30">
+            <div class="container max-w-screen-lg mx-auto">
+                <div class="grid grid-cols-1 sm:grid-cols-2 gap-4 content-center">
+                    <div class="">
+                        <p class=" font-light text-white mb-4">
+                            Thank you for your attention.
+                        </p>
+                        <p class=" font-light text-white">
+                            Sumatera Utara Ibukotanya Medan<br />
+                            Tempat Bermukim Sultan Deli<br />
+                            Terima Kasih Kawan" Sekalian<br />
+                            Semoga Kita Dapat Berjumpa Nanti<br />
+                        </p>
+                    </div>
                     <div class="col-span-2 sm:col-span-1 sm:my-auto  transition-all duration-500">
                         <div class="text-lg font-extralight grid gap-5 transition-all duration-500">
-              <div>
+                            <div>
                                 <a target="blank"
-                  href="https://mail.google.com/mail/u/0/?fs=1&tf=cm&to=yudhatriya07@gmail.com"
+                                    href="https://mail.google.com/mail/u/0/?fs=1&tf=cm&to=yudhatriya07@gmail.com"
                                     class="flex items-center gap-2 text-white">
                                     <ion-icon class="text-xl" name="mail-outline"></ion-icon>ariga@gmail.com
                                 </a>
-              </div>
-            </div>
-          </div>
-        </div>
-        <hr class="my-6  sm:mx-auto text-white lg:my-8" />
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <hr class="my-6  sm:mx-auto text-white lg:my-8" />
                 <span class="block text-sm text-white sm:text-center">© 2022 Ariga Nursing Management™. All Rights
                     Reserved.
-    </span>
-      </div>
-    </div>
-      </section>
-    
- {{-- @vite('resources/js/script.js') --}}
+                </span>
+            </div>
+        </div>
+    </section>
+
+    {{-- @vite('resources/js/script.js') --}}
 
 </body>
 
-<script> //Scrip Untuk Gambar Welcome Page
-  //carousel
-let defaultTransform = 0;
-function goNext() {
-    defaultTransform = defaultTransform - 398;
-    var slider = document.getElementById("slider");
-    if (Math.abs(defaultTransform) >= slider.scrollWidth / 1.7)
-        defaultTransform = 0;
-    slider.style.transform = "translateX(" + defaultTransform + "px)";
-}
-next.addEventListener("click", goNext);
-function goPrev() {
-    var slider = document.getElementById("slider");
-    if (Math.abs(defaultTransform) === 0) defaultTransform = 0;
-    else defaultTransform = defaultTransform + 398;
-    slider.style.transform = "translateX(" + defaultTransform + "px)";
-}
-prev.addEventListener("click", goPrev);
+<script>
+    //Scrip Untuk Gambar Welcome Page
+    //carousel
+    let defaultTransform = 0;
+
+    function goNext() {
+        defaultTransform = defaultTransform - 398;
+        var slider = document.getElementById("slider");
+        if (Math.abs(defaultTransform) >= slider.scrollWidth / 1.7)
+            defaultTransform = 0;
+        slider.style.transform = "translateX(" + defaultTransform + "px)";
+    }
+    next.addEventListener("click", goNext);
+
+    function goPrev() {
+        var slider = document.getElementById("slider");
+        if (Math.abs(defaultTransform) === 0) defaultTransform = 0;
+        else defaultTransform = defaultTransform + 398;
+        slider.style.transform = "translateX(" + defaultTransform + "px)";
+    }
+    prev.addEventListener("click", goPrev);
 </script>
 
-<script> //Scrip Untuk Navbar
-//hamburger
-const hamburger = document.querySelector("#hamburger");
-const navMenu = document.querySelector("#nav-menu");
+<script>
+    //Scrip Untuk Navbar
+    //hamburger
+    const hamburger = document.querySelector("#hamburger");
+    const navMenu = document.querySelector("#nav-menu");
 
-hamburger.addEventListener("click", function () {
+    hamburger.addEventListener("click", function() {
         hamburger.classList.toggle("hamburger-active");
         navMenu.classList.toggle("hidden");
     });
     const navProfile = document.querySelector("#nav-profile");
     const profileNav = document.querySelector("#profilenav");
 
-profileNav.addEventListener("click", function () {
-    navProfile.classList.toggle("hidden");
-});
+    profileNav.addEventListener("click", function() {
+        navProfile.classList.toggle("hidden");
+    });
 </script>
 
-<script>//Scrip Untuk Navbar
-//navbar fixed
-window.onscroll = function () {
-    const header = document.querySelector("header");
-    const navfix = header.offsetTop;
+<script>
+    //Scrip Untuk Navbar
+    //navbar fixed
+    window.onscroll = function() {
+        const header = document.querySelector("header");
+        const navfix = header.offsetTop;
 
         if (window.pageYOffset > navfix) {
             header.classList.add("navbar-fixed");
@@ -190,7 +195,6 @@ window.onscroll = function () {
             header.classList.remove("navbar-fixed");
         }
     };
-
 </script>
 
 <script>
@@ -205,4 +209,6 @@ window.onscroll = function () {
     };
 </script>
 
-<script src='https://cdn.jsdelivr.net/npm/botman-web-widget@0/build/js/widget.js'></script
+<script src='https://cdn.jsdelivr.net/npm/botman-web-widget@0/build/js/widget.js'></script>
+
+</html>
