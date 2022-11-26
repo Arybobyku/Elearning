974dbeff41e985bb0a514f82172c096651221ad0
arybobysiregar <aryboby1@gmail.com>
2022-11-26 12:04:34 +0700

    change position submit alos bor and toi

--- resources/views/general/alos.blade.php
+++ resources/views/general/alos.blade.php
@@ -22,7 +22,11 @@
                     placeholder="Jumlah Pasien Keluar Hidup + Mati" required>
             </div>
         </div>
-        <div class="flex flex-row m-16 gap-8 justify-center items-center">
+        <div class="flex flex-row justify-center items-center">
+            <button type="submit"
+                class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Submit</button>
+        </div>
+        <div class="flex flex-row m-8 gap-8 justify-center items-center">
 
             <div class="mb-6">
                 <label for="hariwaktu" class="block mb-2 text-sm font-medium text-gray-900 ">Hasil</label>
@@ -31,8 +35,6 @@
                     placeholder="" disabled>
             </div>
 
-            <button type="submit"
-                class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Submit</button>
         </div>
 
     </form>
--- resources/views/general/bor.blade.php
+++ resources/views/general/bor.blade.php
@@ -29,8 +29,18 @@
                     class="form-control bg-gray-50 border border-gray-300 text-black text-sm rounded-lg block w-half p-2.5"
                     placeholder="" required>
             </div>
+
         </div>
-        <div class="flex flex-row m-16 gap-8 justify-center items-center">
+
+        <div class="flex flex-row gap-8 justify-center items-center">
+
+            <button type="submit"
+                class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Submit</button>
+
+
+        </div>
+
+        <div class="flex flex-row m-8 gap-8 justify-center items-center">
 
             <div class="mb-6">
                 <label for="hariwaktu" class="block mb-2 text-sm font-medium text-gray-900 ">Hasil</label>
@@ -39,8 +49,7 @@
                     placeholder="" disabled>
             </div>
 
-            <button type="submit"
-                class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Submit</button>
+
         </div>
 
     </form>
